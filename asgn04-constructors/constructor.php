e93696fa5643e4afbab5d7470e422139c63bd8dc
ashleerollice@MB-P <ashleerollice@students.abtech.edu>
2024-09-15 20:02:20 -0400

    Asgn-04 first part of assignment completed

---
+++ asgn04-constructors/constructor.php
@@ -0,0 +1,25 @@
+<?php
+
+class Bird
+{
+  public $commonName;
+  public $latinName;
+
+  //Constructor with two arguments
+  public function  __construct($commonName, $latinName)
+  {
+    $this->commonName = $commonName;
+    $this->latinName = $latinName;
+  }
+}
+
+$robin = new Bird('American Robin', 'Turdus migratorius');
+$easternTowhee = new Bird('Eastern Towhee', 'Pipilo erythrophthalmus');
+
+echo "Common Name: " . $robin->commonName . "<br/>";
+echo "Latin Name: " . $robin->latinName . "<br/>";
+
+echo "<hr/>";
+
+echo "Common Name: " . $easternTowhee->commonName . "<br/>";
+echo "Latin Name: " . $easternTowhee->latinName . "<br/>";
